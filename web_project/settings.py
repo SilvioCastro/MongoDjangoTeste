106e49742182d1ae2c16a80f53ab83542c2d81b0
SilvioCastro <silvioarantescastro@gmail.com>
2021-10-09 23:38:07 -0300

    .

--- web_project/settings.py
+++ web_project/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/3.2/ref/settings/
 """
 
 from pathlib import Path
+import dj_database_url
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -23,9 +24,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-hko#jgj5%9zf3qaw_3-ye^9g5uu=#^q7^$vd6*$!(ve33tipua'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
-ALLOWED_HOSTS = ["mongodjangoteste.herokuapp.com/", "127.0.0.1"]
+ALLOWED_HOSTS = [] # ["mongodjangoteste.herokuapp.com/", "127.0.0.1"]
 
 
 # Application definition
